32336ced5d820e904d6167becc9e9b6f97290686
fuhaiq <fuhaiq@gmail.com>
2025-09-30 18:30:23 +0800

    新增Mybatis乐观锁自动版本获取插件+更新代码生成模版
    新增Mybatis乐观锁自动版本获取插件+更新代码生成模版

--- fund-common/src/main/java/org/fhq/common/mybatis/MybatisConfiguration.java
+++ fund-common/src/main/java/org/fhq/common/mybatis/MybatisConfiguration.java
@@ -3,7 +3,6 @@ package org.fhq.common.mybatis;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,9 +11,10 @@ public class MybatisConfiguration {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor()); // 乐观锁
+//        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor()); // 乐观锁
         interceptor.addInnerInterceptor(new MybatisPaginationInnerInterceptor(DbType.POSTGRE_SQL)); // 分页
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor()); // 防止全表更新与删除
+        interceptor.addInnerInterceptor(new AutoOptimisticLockerInnerInterceptor()); // 自动乐观锁
         return interceptor;
     }
 
